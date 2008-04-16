b5f900e4f7886a4a6cbc2c6469d02cc8c06921b5
Avraham Lembke <blambi@chebab.com>
2008-04-16 22:03:33 +0200

    some more parsing (+ the parser)

--- main.cpp
+++ main.cpp
@@ -81,6 +81,7 @@ int main( int argc, char **argv )
         }
     }
     
+    delete protocol;
     delete net;
     delete ui;
     return EXIT_SUCCESS;
---
+++ protocol.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include "socktroll.hpp"
+
+Protocol::Protocol( void )
+{
+    nick = std::string( "" );
+    authed = false;
+}
+
+Protocol::~Protocol( void )
+{
+    if( net and authed )
+        net->send( "quit" );
+}
+
+void Protocol::parse( std::string message )
+{
+    int del;
+    std::string temp;
+
+    /* FIXME: not that pretty but it works */
+    if( message.substr( 0, 3 ) == "msg" )
+    {
+        temp = message.substr( 4 );
+        temp = temp.substr( 0, temp.find( " " ));
+            ui->print( "<%s> %s", temp.c_str(),
+                       message.substr( 5 + temp.size() ).c_str() );
+    }
+    else if( message.substr( 0, 1 ) == "+" or message.substr( 0, 1 ) == "-" )
+        ui->print( " %s %s", message.substr( 0, 1 ).c_str(),
+                   message.substr( 2 ).c_str() );
+    else
+        ui->print( "UNKOWN: '%s'", message.c_str() );
+}
+
+void Protocol::msg( std::string message )
+{
+    net->send( std::string( "msg " ) + message );
+}
+
+void Protocol::cmd( std::string command )
+{
+    /* FIXME: make me do something! */
+    net->send( command.erase( 0, 1 ) );
+}
